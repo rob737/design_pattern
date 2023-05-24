7a15040595e8236e050f32193dfd5851e04858ff
rob737 <robinsrivastava737@gmail.com>
2023-05-24 08:11:43 +0530

    24-05-23 : classification class

---
+++ src/main/java/pattern/interfaces/Classification_1.java
@@ -0,0 +1,9 @@
+package pattern.interfaces;
+
+public class Classification_1 {
+    public static final Classification_1 consumer = new Classification_1();
+    public static final Classification_1 display = new Classification_1();
+    private Classification_1(){
+
+    }
+}
