6dbb329e62556569e4600152ae368baa160d09f3
rob737 <robinsrivastava737@gmail.com>
2023-06-07 07:33:02 +0530

    07-06-23 : State pattern use case

--- src/main/java/pattern/behavioural/state/ToolActionRelease.java
+++ src/main/java/pattern/behavioural/state/ToolActionRelease.java
@@ -1,5 +1,9 @@
 package pattern.behavioural.state;
 
+/*
+* Instead of interface (my solution), alternative was to use abstract class but since we don't have to
+provide any common method to all the underlying classes so we didn't use it.
+* */
 public interface ToolActionRelease {
     void performOnMouseRelease();
 }
