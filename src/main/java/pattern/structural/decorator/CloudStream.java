a19c2a3635945c8dcbfdcbf9325470deba5121e6
rob737 <robinsrivastava737@gmail.com>
2024-01-31 10:33:50 +0530

    31-01-24 : decorator pattern

--- src/main/java/pattern/structural/decorator/CloudStream.java
+++ src/main/java/pattern/structural/decorator/CloudStream.java
@@ -4,7 +4,7 @@ package pattern.structural.decorator;
 * Use case : you want to re-use the operation (e.g. write method) but
 * the input to operation can vary.
 * We can leverage decorator pattern to decorate the input to
-* transform it into our custom format and also re-using the underlying implementation.
+* transform it into our custom format and also re-use the underlying implementation.
 * */
 public class CloudStream implements CloudStreamPlaceholder{
     @Override
--- src/main/java/pattern/structural/decorator/CloudStreamPlaceholder.java
+++ src/main/java/pattern/structural/decorator/CloudStreamPlaceholder.java
@@ -1,5 +1,8 @@
 package pattern.structural.decorator;
 
+/*
+* Component Interface with operation method as write.
+* */
 public interface CloudStreamPlaceholder {
     void write(String text);
 }
